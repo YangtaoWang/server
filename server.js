681e19918bc2d89dc86b4633019efa24af20f2f4
YangtaoWang <peterwang199205@163.com>
2019-04-26 12:08:12 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('ich liebe dich, mein Schaetzchen!')
+}).listen(8081)
+
+console.log('server running on http://39.105.111.73:8081')
