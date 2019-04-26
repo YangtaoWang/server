660b23b49c1c0b0ef56a19e716fd86339bdd5287
YangtaoWang <peterwang199205@163.com>
2019-04-26 14:58:50 +0800

    initpm2

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const http = require('http')
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('ich liebe dich, mein Schaetzchen!')
-}).listen(8081)
+}).listen(3000)
 
 console.log('server running on http://39.105.111.73:8081')
